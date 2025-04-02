fab0b350cfb5afbe703f86f6eb7c06d77f784daf
MasObima <bimaseptian308@gmail.com>
2025-04-03 06:43:09 +0700

    rekyasa sedikit

--- api/config/multer.js
+++ api/config/multer.js
@@ -3,33 +3,16 @@ const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const multer = require('multer');
 require('dotenv').config();
 
-// Konfigurasi penyimpanan file
-// const storage = multer.diskStorage({
-//     destination: (req, file, cb) => {
-//         cb(null, 'uploads/products'); // Folder penyimpanan
-//     },
-//     filename: (req, file, cb) => {
-//         cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-//     }
-// });
-
-// const fileFilter = (req, file, cb) => {
-//     if (file.mimetype.startsWith('image/')) {
-//         cb(null, true);
-//     } else {
-//         cb(new Error('File harus berupa gambar!'), false);
-//     }
-// };
-
+// Konfigurasi penyimpanan menggunakan Cloudinary
 const storage = new CloudinaryStorage({
-    cloudinary: cloudinary,
-    params: {
-      folder: 'product', 
-      allowed_formats: ['jpg', 'png', 'jpeg'],
-    },
-  });
+  cloudinary: cloudinary,
+  params: {
+    folder: 'product', // Nama folder di Cloudinary
+    allowed_formats: ['jpg', 'png', 'jpeg'], // Format gambar yang diizinkan
+  },
+});
 
-
-const upload = multer({ storage, fileFilter });
+// Middleware multer untuk menangani upload
+const upload = multer({ storage });
 
 module.exports = upload;
